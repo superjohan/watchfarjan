88325240391dc1a39d354c73fc14c16a4e8d6235
Johan Halin <johan@halin.me>
2018-09-23 08:19:15 +0300

    Initial Commit

---
+++ watchfarjanholder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  watchfarjanholder
+//
+//  Created by Johan Halin on 23/09/2018.
+//  Copyright © 2018 Jumalauta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
